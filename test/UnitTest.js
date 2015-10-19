0fb63941eb0d646a6f643795cee990537d6d27c8
Olivia Huang <oliviahyacinth@gmail.com>
2015-10-19 13:36:08 -0400

    add unit tests using mocha and chai

--- test/UnitTest.js
+++ test/UnitTest.js
@@ -2,6 +2,9 @@ var chai = require('chai')
   , expect = chai.expect
   , should = chai.should();
 var subject = require("../subject.js");
+var fs = require("fs");
+var mock = require('mock-fs');
+
 
 describe("subject",function(){
 	describe("inc function", function(){
@@ -56,27 +59,61 @@ describe("subject",function(){
 		});	
 	});
 	describe("fileTest funcion", function(){
-		it("return 1 when x>7, y<0", function(){
-			var weird1 = subject.weird(8,-1,0,'whatever');
-			expect(weird1).to.equal(1);
+		it("return false when there is no file under path/fileExists", function(){
+			mock({"path/fileExists":{},"pathContent":{"file1":"text content"}});
+			subject.fileTest('path/fileExists','pathContent/file1');
+			var fileTestResult1 = subject.fileTest('path/fileExists','pathContent/file1'); 
+			expect(fileTestResult1).to.equal(false);
+			mock.restore();
 		});
-		it("return 1 when z<42", function(){
-			var weird1 = subject.weird(0,0,41,'strict');
-			expect(weird1).to.equal(0);
-		});
-		it("return 1 when z<42 and mode[0]==werw ", function(){
-			var weird1 = subject.weird(0,0,41,['werw']);
-			expect(weird1).to.equal(1);
-		});
-		it("return 1 when (x<=7 or y>=0) and z>42 and mode!=strict ", function(){
-			var weird1 = subject.weird(7,0,42,'notstrict');
-			expect(weird1).to.equal(6);
+		it("return false when file in pathContent/file1 has no content", function(){
+			mock({"path/fileExists":{"file1":"text content"},"pathContent":{"file1":""}});
+			subject.fileTest('path/fileExists','pathContent/file1');
+			var fileTestResult1 = subject.fileTest('path/fileExists','pathContent/file1'); 
+			expect(fileTestResult1).to.equal(false);
+			mock.restore();
 		});
 
-		it("return 1 when (x<=7 or y>=0) and z>42 and mode==strict ", function(){
-			var weird1 = subject.weird(0,0,42,'strict');
-			expect(weird1).to.equal(1);
-		});	
+		it("return true when there is file with content in filePath", function(){
+			mock({"path/fileExists":{"file1":"text content"},"pathContent":{"file1":"text content"}});
+			subject.fileTest('path/fileExists','pathContent/file1');
+			var fileTestResult1 = subject.fileTest('path/fileExists','pathContent/file1'); 
+			expect(fileTestResult1).to.equal(true);
+			mock.restore();
+		});
+
+	});
+	describe("normalize funcion", function(){
+		it("return raw phone number when match", function(){
+			var normalized1 = subject.normalize('(123)-456-7890'); 
+			expect(normalized1).to.equal('1234567890');
+		});
+		it("return input when doesn't match", function(){
+			var normalized1 = subject.normalize('abc'); 
+			expect(normalized1).to.equal('abc');
+		});
+	});
+	describe("format funcion", function(){
+		it("return format", function(){
+			var normalized1 = subject.format('1234567890','(NNN)NNN-NNNN',true); 
+			expect(normalized1).to.equal('(123)456-7890');
+		});
+		it("return format", function(){
+			var normalized1 = subject.format('(123)456-7890','(NNN)NNN-NNNN',false); 
+			expect(normalized1).to.equal('(123)456-7890');
+		});
+
+	});
+	describe("blackListNumber funcion", function(){
+		it("return true when area code is 212", function(){
+			var blacklist1 = subject.blackListNumber('2124567890'); 
+			expect(blacklist1).to.equal(true);
+		});
+		it("return false when area code is not 212", function(){
+			var blacklist2 = subject.blackListNumber('1234567890'); 
+			expect(blacklist2).to.equal(false);
+		});
+
 	});
 
 });
