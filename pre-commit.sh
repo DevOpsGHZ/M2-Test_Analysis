e1a35eb5456b87e42681972d3b91f7ca2deb851d
Kelei Gong <kgong@ncsu.edu>
2015-10-19 22:17:34 -0400

    update pre commit script

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,6 @@
-#!bin/bash
-
+#!/bin/bash
+# echo "Running pre-commit script"
+# sh ./pre-commit.sh
 echo "----------------Pre Commit script----------------"
 sleep 1
 echo "----------------Running UnitTest ----------------"
@@ -21,6 +22,32 @@ sleep .5
 node generate.js
 node_modules/.bin/istanbul cover test.js > coverage.txt
 cat coverage.txt
+sed -n '3,6p' coverage.txt > report.txt
+while read line
+do
+    # length=$(echo -n "$line" | wc -c)
+    l="5"
+    var="50"
+    hundred="100"
+    temp=$line
+    t=${temp:15:3}
+    if [ "$t" == "$hundred" ];then
+        t="99."
+    fi
+
+
+    t1=${t:0:2}
+    # echo $length
+    if [ "$t1" -gt "$var" ];then
+        echo ${temp:0:10} "Coverage is higher than " $var "% Pass!"
+    else 
+        echo ${temp:0:10} "Coverage is less than " $var "%."
+        echo "Failed to commit" 
+        exit 1
+    fi
+done < report.txt
+
+
 echo "-----------Running Security Checking-------------"
 sleep .5
 git status > stage.txt
@@ -29,13 +56,18 @@ node security_checking.js
 rm unitTestResults.txt
 rm coverage.txt
 rm stage.txt
+rm report.txt
+
+exec < /dev/tty
 
 while true; do
     read -p "Do you wish to perform this Commit? [Y/n]" yn
+    if [ "$yn" = "" ]; then
+        yn='Y'
+    fi
     case $yn in
         [Yy]* ) break;;
-        [Nn]* ) echo "Commit discard!";exit;;
-        * ) echo "Please answer yes or no.";;
+        [Nn]* ) echo "Commit discard!";exit 1;;
+    * ) echo "Please answer yes or no.";;
     esac
 done
-echo "Commit test passed!"
