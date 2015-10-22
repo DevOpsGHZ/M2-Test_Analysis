96602ff493d6665abdb2c8608789e7d80f8da227
tastynoodle <szhang29@ncsu.edu>
2015-10-22 14:54:03 -0400

    add comment radio analysis

---
+++ comments_radio.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import re
+
+with open("subject.js", "r") as f:
+	buf = f.read()
+
+comment_num = 0
+pattern1 = r"//.*\n"
+#print pattern1.match(buf)
+comment_num += len( re.findall(pattern1, buf) )
+
+pattern2 = r"/\*[^*]*\*/"
+res = re.findall(pattern2, buf)
+comment_num += len(res)
+
+for i in res:
+	comment_num += len(re.findall(r"\n", i))
+
+with open("subject.js", "r") as f:
+	buf = f.readlines()
+
+code_num = len(buf)
+
+print "Code: %d lines, Comment: %d lines, Ratio:%.2f%%" % ( code_num, comment_num, float(comment_num)/code_num)
