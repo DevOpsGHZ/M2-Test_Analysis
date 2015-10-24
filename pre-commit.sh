507b432cf4ccf2b35d6cabcc8be7184938f06879
Kelei Gong <kgong@ncsu.edu>
2015-10-23 20:17:24 -0400

    update pre-commit script

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# echo "Running pre-commit script"
-# sh ./pre-commit.sh
+
 echo "--------------------Pre Commit script--------------------"
 sleep 1
 
@@ -20,6 +19,43 @@ else
     rm unitTestResults.txt
 	echo "All unit test cases passed!"
 fi
+
+
+echo
+echo "------------------- Running Istanbul --------------------"
+sleep 1.5
+echo "Generating test cases ......"
+sleep 1
+node generate.js
+node_modules/.bin/istanbul cover test.js > coverage.txt
+cat coverage.txt
+sed -n '3,6p' coverage.txt > report.txt
+rm coverage.txt
+while read line
+do
+    # length=$(echo -n "$line" | wc -c)
+    l="5"
+    var="50"
+    hundred="100"
+    temp=$line
+    t=${temp:15:3}
+    if [ "$t" == "$hundred" ];then
+        t="99."
+    fi
+
+
+    t1=${t:0:2}
+    # echo $length
+    if [ "$t1" -gt "$var" ];then
+        echo ${temp:0:10} "Coverage is higher than " $var "% Pass!"
+    else 
+        echo ${temp:0:10} "Coverage is less than " $var "%."
+        echo "Failed to commit" 
+        exit 1
+    fi
+done < report.txt
+rm report.txt
+
 
 echo "---------Running JSHint to analyse source code ----------"
 sleep 1.5
@@ -67,40 +103,7 @@ else
     echo "No js file to be committed"
 fi
 
-echo
-echo "------------------- Running Istanbul --------------------"
-sleep 1.5
-echo "Generating test cases ......"
-sleep 1
-node generate.js
-node_modules/.bin/istanbul cover test.js > coverage.txt
-cat coverage.txt
-sed -n '3,6p' coverage.txt > report.txt
-rm coverage.txt
-while read line
-do
-    # length=$(echo -n "$line" | wc -c)
-    l="5"
-    var="50"
-    hundred="100"
-    temp=$line
-    t=${temp:15:3}
-    if [ "$t" == "$hundred" ];then
-        t="99."
-    fi
 
-
-    t1=${t:0:2}
-    # echo $length
-    if [ "$t1" -gt "$var" ];then
-        echo ${temp:0:10} "Coverage is higher than " $var "% Pass!"
-    else 
-        echo ${temp:0:10} "Coverage is less than " $var "%."
-        echo "Failed to commit" 
-        exit 1
-    fi
-done < report.txt
-rm report.txt
 
 echo
 echo "-------------Running Comments Ratio Checking---------------"
@@ -116,21 +119,22 @@ node security_checking.js
 rm stage.txt
 rm staged_file.txt
 echo
-# rm unitTestResults.txt
-# rm coverage.txt
-# rm stage.txt
-# rm report.txt
 
-# exec < /dev/tty
+rm unitTestResults.txt
+rm coverage.txt
+rm stage.txt
+rm report.txt
 
-# while true; do
-#     read -p "Do you wish to perform this Commit? [Y/n]" yn
-#     if [ "$yn" = "" ]; then
-#         yn='Y'
-#     fi
-#     case $yn in
-#         [Yy]* ) break;;
-#         [Nn]* ) echo "Commit discard!";exit 1;;
-#     * ) echo "Please answer yes or no.";;
-#     esac
-# done
+exec < /dev/tty
+
+while true; do
+    read -p "Do you wish to perform this Commit? [Y/n]" yn
+    if [ "$yn" = "" ]; then
+        yn='Y'
+    fi
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Commit discard!";exit 1;;
+    * ) echo "Please answer yes or no.";;
+    esac
+done
